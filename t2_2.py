19dc2251132a220f66aef97abd7d1f689d3c1e61
kttur <kot7672@gmail.com>
2019-12-18 00:51:36 +0300

    T2-2 fix

--- t2_2.py
+++ t2_2.py
@@ -14,7 +14,7 @@ class MRAverageWordLength(MRJob):
     def reducer(self, _, lengths):
 	    total = sum(lengths)
         yield (float(total[0])/total[1], total[1])
-		
-		
+
+
 if __name__ == "__main__":
     MRAverageWordLength.run()
