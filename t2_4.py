3f724c7aea1cd33269b898eac3b90d9db3fb2f52
kttur <kot7672@gmail.com>
2019-12-18 02:29:05 +0300

    T2-4 tabs fix

--- t2_4.py
+++ t2_4.py
@@ -12,13 +12,13 @@ class MRUppercaseWords10(MRJob):
             yield word.lower(), word[0].isupper()
 
     def reducer(self, word, upper_counters):
-	    total, upper = 0, 0
-		for c in upper_counters:
-		    total += 1
-			if c:
-			    upper += 1
-		if total >= 10 and float(upper)/total >= 0.5:
-			yield word, str(total, upper)
+        total, upper = 0, 0
+        for c in upper_counters:
+            total += 1
+            if c:
+                upper += 1
+        if total >= 10 and float(upper)/total >= 0.5:
+            yield word, str(total, upper)
 
 
 if __name__ == "__main__":
