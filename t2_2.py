ea5b9e5aa125ed92bf2102dbe12a049579456a7b
Artem <kot7672@gmail.com>
2019-12-17 23:58:58 +0300

    T2-2 new words counting method

--- t2_2.py
+++ t2_2.py
@@ -9,10 +9,10 @@ class MRAverageWordLength(MRJob):
 
     def mapper(self, _, line):
         for word in WORD_RE.findall(line):
-            yield None, len(word)
+            yield None, (len(word), 1)
 
     def reducer(self, _, lengths):
-        yield "avg len: %f" % float(sum(lengths))/len(lengths), "total words: %i" % len(lengths)
+        yield "avg len: %f" % float(sum(lengths[0]))/sum(lengths[1]), "total words: %i" % sum(lengths[1])
 		
 		
 if __name__ == "__main__":
