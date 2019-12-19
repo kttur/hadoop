bc410ef82f4b26414aa225dd18b21f6ed1e9d3be
kttur <kot7672@gmail.com>
2019-12-20 01:45:40 +0300

    Add t2.sh

---
+++ t2.sh
@@ -0,0 +1,12 @@
+echo "t2_1"
+python t2_1.py wiki.txt -o results/t2_1
+echo "t2_2"
+python t2_2.py wiki.txt -o results/t2_2
+echo "t2_3"
+python t2_3.py wiki.txt -o results/t2_3
+echo "t2_4"
+python t2_4.py wiki.txt -o results/t2_4
+echo "t2_5"
+python t2_5.py wiki.txt -o results/t2_5
+echo "t2_6"
+python t2_6.py wiki.txt -o results/t2_6
