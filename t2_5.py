765bb0e972b36dc0ce3070e7d5378104aeead87f
kttur <kot7672@gmail.com>
2019-12-19 17:35:43 +0300

    T2-5, T2-6

---
+++ t2_5.py
@@ -0,0 +1,27 @@
+from mrjob.job import MRJob
+import re
+
+
+class MROneDotAbbr(MRJob):
+
+    PATTERN_RE = re.compile(r'( |^)\w+\.[,;:"?!]?( |$)"?\w?')
+    ABBR_RE = re.compile(r'\w+\.')
+    REMOVED_SYMBOLS = re.compile('[,;:"?!]+')
+
+    def mapper(self, _, line):
+        for match in PATTERN_RE.findall(line):
+		    clean_match = re.sub(r'[,;:"?!]+', '', match)
+            yield ABBR_RE.search(match).lower(), clean_match[-1].isupper()
+
+    def reducer(self, word, upper_counters):
+        total, upper = 0, 0
+        for c in upper_counters:
+            total += 1
+            if c:
+                upper += 1
+        if float(upper)/total <= 0.5:
+            yield word, str((total, upper))
+
+
+if __name__ == "__main__":
+    MROneDotAbbr.run()
---
+++ t2_6.py
@@ -0,0 +1,13 @@
+from mrjob.job import MRJob
+from t2_5 import MROneDotAbbr
+import re
+
+
+class MROneLetterAbbr(MROneDotAbbr):
+
+    PATTERN_RE = re.compile(r'( |^)(\w\.)+[,;:"?!]?( |$)"?\w?')
+    ABBR_RE = re.compile(r'(\w\.)+')
+
+
+if __name__ == "__main__":
+    MROneLetterAbbr.run()
